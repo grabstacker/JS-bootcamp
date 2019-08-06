e5ceecd58c00226645d068fabe56d35d1694f6e4
grabstacker <bradkay12@gmail.com>
2019-08-07 10:07:32 +1200

    Created a function to add two numbers together

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 console.log('Hello world')
 var bradsVariable = 'Brads vars'
 console.log(bradsVariable)
-function x(){
-
+function add(var1,var2){
+return var1 + var2
 }
+console.log(add(44,33))
  bla = 2
 
  console.log(bla)
