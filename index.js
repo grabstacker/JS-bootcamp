28f20c138cc3463e41fd5b92d3a50fe62c6782dc
grabstacker <bradkay12@gmail.com>
2019-08-09 16:35:05 +1200

    Switch case statement

--- index.js
+++ index.js
@@ -27,3 +27,19 @@ if(a || b){
  }
 }
 myfunction()
+
+var a = 3
+switch (a) {
+    case 3:
+      console.log( 'Too small' );
+      break;
+    case 4:
+        console.log( 'Exactly!' );
+        break;
+    case 5:
+        console.log( 'Too big' );
+        break;
+    default:
+        console.log( "I don't know such values" );
+        break;
+  }
